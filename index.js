5c370b32fab6159dba682b6c650b7d02e53ef38d
Nathally <nathallyccd@gmail.com>
2022-10-22 11:13:59 -0300

    Adicionando localStorage

--- index.js
+++ index.js
@@ -5,9 +5,28 @@ const tarefa = (id, novaTarefa) => `<div id='${id}'>
 <input type="checkbox" />
 </div>`
 
+function exibirLista() {
+    id++
+    const tarefas = JSON.parse(localStorage.getItem('lista-tarefas'))
+    if(tarefas){
+        tarefas.forEach(tarefaListada => {
+            document.querySelector('#lista-tarefas').innerHTML += tarefa(id, tarefaListada)
+        })
+    }
+}
+
 function adicionarTarefa(){
     id++
     const novaTarefa = document.getElementById('nome-tarefa').value
     document.querySelector('#lista-tarefas').innerHTML += tarefa(id, novaTarefa)
+    const listaTarefas = localStorage.getItem('lista-tarefas')
+    if(listaTarefas){
+        const novaLista = JSON.parse(listaTarefas)
+        novaLista.push(novaTarefa)
+        localStorage.setItem('lista-tarefas', JSON.stringify(novaLista))
+    } else {
+        localStorage.setItem('lista-tarefas', JSON.stringify([novaTarefa]))
+    }
 }
 
+exibirLista()
