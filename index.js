3cdec656e3e88853c0e63682edbb83241d9db836
Nathally <nathallyccd@gmail.com>
2022-10-25 20:19:25 -0300

    Removendo tarefas

--- index.js
+++ index.js
@@ -1,25 +1,46 @@
 let id = 0
 
-const tarefa = (id, novaTarefa) => `<div id='${id}'>
-<p>${novaTarefa}</p>
+const tarefa = (id, novaTarefa) => `<div>
+<p id='${id}'>${novaTarefa}</p>
 <input type="checkbox" />
+<button onclick="removerTarefa(${id})">Remover</button>
 </div>`
 
 function exibirLista() {
-    id++
+    console.log('called')
     const tarefas = JSON.parse(localStorage.getItem('lista-tarefas'))
     if(tarefas){
         tarefas.forEach(tarefaListada => {
+            id++
             document.querySelector('#lista-tarefas').innerHTML += tarefa(id, tarefaListada)
         })
     }
 }
 
+const validarTarefa = (novaTarefa) => {
+    let tarefaExistente = false
+    const listaTarefas = JSON.parse(localStorage.getItem('lista-tarefas'))
+
+    listaTarefas.map(tarefa => {
+        if(tarefa === novaTarefa){
+            tarefaExistente = true
+            alert('Tarefa já existente')
+        }
+    })
+
+    return tarefaExistente
+}
+
 function adicionarTarefa(){
     id++
     const novaTarefa = document.getElementById('nome-tarefa').value
-    document.querySelector('#lista-tarefas').innerHTML += tarefa(id, novaTarefa)
     const listaTarefas = localStorage.getItem('lista-tarefas')
+    if(validarTarefa(novaTarefa)){
+        return
+    }
+
+    document.querySelector('#lista-tarefas').innerHTML += tarefa(id, novaTarefa)
+
     if(listaTarefas){
         const novaLista = JSON.parse(listaTarefas)
         novaLista.push(novaTarefa)
@@ -27,6 +48,15 @@ function adicionarTarefa(){
     } else {
         localStorage.setItem('lista-tarefas', JSON.stringify([novaTarefa]))
     }
+}
+
+const removerTarefa = (id) => {
+    const tarefaDeletada = document.getElementById(id).innerHTML
+    const listaTarefas = JSON.parse(localStorage.getItem('lista-tarefas'))
+    const novaListaTarefa = listaTarefas.filter(tarefa => tarefa !== tarefaDeletada)
+    localStorage.setItem('lista-tarefas', JSON.stringify(novaListaTarefa))
+    document.querySelector('#lista-tarefas').innerHTML = ''
+    exibirLista()
 }
 
 exibirLista()
