d41380ec8037bea5def8eddbc1fe926287e90e04
Nathally <nathallyccd@gmail.com>
2022-10-27 21:35:02 -0300

    Quebrando a aplicação

--- index.js
+++ index.js
@@ -1,23 +1,47 @@
 let id = 0
 
+const dados = {
+    pegarTarefas: () => JSON.parse(localStorage.getItem('lista-tarefas')),
+    editarTarefas: (listaTarefas) => localStorage.setItem('lista-tarefas', JSON.stringify(listaTarefas))
+}
+
 const tarefa = (id, novaTarefa) => `<div>
-<p id='${id}'>${novaTarefa}</p>
-<input type="checkbox" onchange="marcarTarefa(${id})"/>
+<p id='${id}'>
+${novaTarefa.finalizada ? `<strike id="strike${id}">` : ''}
+${novaTarefa.tarefa}
+${novaTarefa.finalizada ? '</strike>' : ''}
+</p>
+<input type="checkbox" ${novaTarefa.finalizada ? 'checked' : ''} onchange="marcarTarefa(${id})"/>
 <button onclick="removerTarefa(${id})">Remover</button>
 </div>`
 
 const marcarTarefa = (id) => {
     const strike = document.getElementById(`strike${id}`)
+    const listaTarefas = dados.pegarTarefas()
+    console.log(strike)
+
     if(strike){
         document.getElementById(id).innerHTML = strike.innerHTML
+        const tarefaPendente = document.getElementById(id).innerHTML.replace(/\\n/g, '')
+        console.log(tarefaPendente)
+        // mudarStatusTarefa(String(tarefaPendente), false)
     } else {
         const tarefaConcluida = document.getElementById(id).innerHTML
-        document.getElementById(id).innerHTML = `<strike id='strike${id}'>${tarefaConcluida}</strike`
+        document.getElementById(id).innerHTML = `<strike id='strike${id}'>${tarefaConcluida}</strike>`
+        const index = listaTarefas.findIndex(tarefaListada => tarefaListada.tarefa === tarefaConcluida)
+        listaTarefas[index].finalizada = true
+        dados.editarTarefas(listaTarefas)
     }
 }
 
+const mudarStatusTarefa = (tarefa, status) => {
+    const index = listaTarefas.findIndex(tarefaListada => tarefaListada.tarefa === tarefa)
+    listaTarefas[index].finalizada = status
+    dados.editarTarefas(listaTarefas)
+}
+
 function exibirLista() {
-    const tarefas = JSON.parse(localStorage.getItem('lista-tarefas'))
+    const tarefas = dados.pegarTarefas()
     if(tarefas){
         tarefas.forEach(tarefaListada => {
             id++
@@ -28,11 +52,11 @@ function exibirLista() {
 
 const validarTarefa = (novaTarefa) => {
     let tarefaExistente = false
-    const listaTarefas = JSON.parse(localStorage.getItem('lista-tarefas'))
+    const listaTarefas = dados.pegarTarefas()
 
     if(listaTarefas){
         listaTarefas.map(tarefa => {
-            if(tarefa === novaTarefa){
+            if(tarefa.tarefa === novaTarefa.tarefa){
                 tarefaExistente = true
                 alert('Tarefa já existente')
             }
@@ -44,7 +68,11 @@ const validarTarefa = (novaTarefa) => {
 
 function adicionarTarefa(){
     id++
-    const novaTarefa = document.getElementById('nome-tarefa').value
+    const novaTarefa = {
+        tarefa: document.getElementById('nome-tarefa').value,
+        finalizada: false
+    }
+    
     const listaTarefas = localStorage.getItem('lista-tarefas')
     if(validarTarefa(novaTarefa)){
         return
@@ -55,17 +83,17 @@ function adicionarTarefa(){
     if(listaTarefas){
         const novaLista = JSON.parse(listaTarefas)
         novaLista.push(novaTarefa)
-        localStorage.setItem('lista-tarefas', JSON.stringify(novaLista))
+        dados.editarTarefas(novaLista)
     } else {
-        localStorage.setItem('lista-tarefas', JSON.stringify([novaTarefa]))
+        dados.editarTarefas([novaTarefa])
     }
 }
 
 const removerTarefa = (id) => {
     const tarefaDeletada = document.getElementById(id).innerHTML
-    const listaTarefas = JSON.parse(localStorage.getItem('lista-tarefas'))
-    const novaListaTarefa = listaTarefas.filter(tarefa => tarefa !== tarefaDeletada)
-    localStorage.setItem('lista-tarefas', JSON.stringify(novaListaTarefa))
+    const listaTarefas = dados.pegarTarefas()
+    const novaListaTarefa = listaTarefas.filter(tarefa => tarefa.tarefa !== tarefaDeletada)
+    dados.editarTarefas(novaListaTarefa)
     document.querySelector('#lista-tarefas').innerHTML = ''
     exibirLista()
 }
